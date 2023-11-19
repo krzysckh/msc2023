1a013d3093bb40f5bcb0156e3cf65ae1f3737672
krzysckh <krzysckh@wp.pl>
2023-11-19 16:49:31 +0100

    fix build.sh

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ LDFLAGS="-lraylib -lm"
 rm ./main
 $CC $CFLAGS $LDFLAGS main.c -o ./main
 
-if [ $BUILD_WINDOWS = "yes" ]; then
+if [ "$BUILD_WINDOWS" = "yes" ]; then
   [ -f "libraylib.a" ] || wget -O libraylib.a https://pub.krzysckh.org/libraylib.a
   x86_64-w64-mingw32-gcc $CFLAGS ./main.c ./libraylib.a \
                          -lm -lopengl32 -lgdi32 -lwinmm -lmingw32 \
