dadf23166f7dd48971c0a78d5951b967b83cfcca
krzysckh <krzysckh@wp.pl>
2023-12-03 14:59:48 +0100

    rc

--- rc.scm
+++ rc.scm
@@ -10,9 +10,10 @@
     (create-mirror 0 300 400 600)))
 
 (define (kp-hook k d)
-  (cond
-    ((eqv? k #\e) (gui/input-popup "eval scheme" loads))
-    ((eqv? k #\q) (exit 0))
-    ((eqv? k #\s) (spawn-things))))
+  (when *keypress-can-be-handled*
+    (cond
+      ((eqv? k #\e) (gui/input-popup "eval scheme" loads))
+      ((eqv? k #\q) (exit 0))
+      ((eqv? k #\s) (spawn-things)))))
 
 (add-hook 'keypress kp-hook)
