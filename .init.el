6f32446f0524f9029847a8acae35609664f9a8d7
krzysckh <krzysckh@wp.pl>
2024-02-09 12:00:12 +0100

    otworz bufory z wszystkimi plikami w .init.el

--- .init.el
+++ .init.el
@@ -1,5 +1,10 @@
 (progn
   (defun conf/prepare ()
+    (setq default-directory "~/programowanko/science-cup-2023")
+    (mapcar #'find-file-noselect (file-expand-wildcards "rc.scm"))
+    (mapcar #'find-file-noselect (file-expand-wildcards "scm/*.scm"))
+    (mapcar #'find-file-noselect (file-expand-wildcards "src/*.[ch]"))
+
     (setq conf/make "make")
     (if (string= (system-name) "openchad") (setq conf/make "gmake"))
 
