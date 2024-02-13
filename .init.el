53bf5859628c858f692c744f6db9bc9c82afbfd5
krzysckh <krzysckh@wp.pl>
2024-02-13 19:11:08 +0100

    C-c C-c C-c, C-c C-p

--- .init.el
+++ .init.el
@@ -1,7 +1,13 @@
 (progn
+  (setq *default-directory* "~/programowanko/science-cup-2023")
+  (defun conf/run-in-default-dir (s async)
+    (cd *default-directory*)
+    (if async
+        (async-shell-command s)
+      (shell-command s)))
+
   (defun conf/prepare ()
-    (setq *default-directory* "~/programowanko/science-cup-2023")
-    (setq default-directory "~/programowanko/science-cup-2023")
+    (setq default-directory *default-directory*)
     (mapcar #'find-file-noselect (file-expand-wildcards "rc.scm"))
     (mapcar #'find-file-noselect (file-expand-wildcards "scm/*.scm"))
     (mapcar #'find-file-noselect (file-expand-wildcards "src/*.[ch]"))
@@ -9,14 +15,27 @@
     (setq buffer-menu #'switch-to-buffer) ; TODO: przeniose to do .emacs
     (setq list-buffers #'switch-to-buffer) ; TODO: przeniose to do .emacs
 
-    (global-set-key (kbd "C-c C-t") (lambda ()
-                                      (interactive)
-                                      (cd *default-directory*)
-                                      (shell-command "etags scm/*.scm src/*.[ch] tinyscheme/*.[ch]")
-                                      (setq tags-file-name (concat default-directory "/TAGS"))))
-
     (setq conf/make "make")
     (if (string= (system-name) "openchad") (setq conf/make "gmake"))
+
+    (global-set-key
+     (kbd "C-c C-t")
+     (lambda ()
+       (interactive)
+       (conf/run-in-default-dir "etags scm/*.scm src/*.[ch] tinyscheme/*.[ch]" nil)
+       (setq tags-file-name (concat default-directory "/TAGS"))))
+
+    (global-set-key
+     (kbd "C-c C-c C-c")
+     (lambda ()
+       (interactive)
+       (conf/run-in-default-dir (concat conf/make " clean") nil)))
+
+    (global-set-key
+     (kbd "C-c C-p")
+     (lambda ()
+       (interactive)
+       (conf/run-in-default-dir (concat conf/make " pubcpy") t)))
 
     (setq compile-command
           (concat
